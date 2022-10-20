18d50388f64a1224c87772f9adcb4c687615ee10
Newer0ne <t80448@gmail.com>
2022-10-20 13:55:29 +0600

    1

--- presentation.py
+++ presentation.py
@@ -19,44 +19,22 @@ def run_query(query):
     return rows
 
 
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0,00'})
+    worksheet.set_column('A:A', None, format1)
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+
+
 link_ads = st.secrets["ads"]
 
-#rows_ads = run_query(f'SELECT * FROM "{link_ads}"')
+rows_ads = run_query(f'SELECT * FROM "{link_ads}"')
 #ads = pd.DataFrame(rows_ads, dtype=str)
 #st.dataframe(data=ads)
-
-import requests
-
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-
-def download_file_from_google_drive(id, destination):
-    URL = link_ads
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)
-
-
-link_test_task = '[Тестовое задание](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)'
-st.markdown(link_test_task, unsafe_allow_html=True)
