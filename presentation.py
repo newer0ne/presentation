d52557b7731d8a05fb34cd33d2a9ff630602ae00
Newer0ne <t80448@gmail.com>
2022-10-20 13:18:24 +0600

    import gsheetsdb

--- presentation.py
+++ presentation.py
@@ -1,14 +1,17 @@
 import streamlit as st
-import pandas
+import pandas as pd
+from gsheetsdb import connect
 
 link_test_task = '[Тестовое задание](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)'
 st.markdown(link_test_task, unsafe_allow_html=True)
 
 conn = connect()
 
-@st.cache(ttl=600)
+@ st.cache(ttl=600)
 
-link_ads = st.secrets["link_ads"]
+
+link_ads = st.secrets["ads"]
+link_ads
 
 def run_query(query):
     rows = conn.execute(query, headers=1)
@@ -17,40 +20,3 @@ def run_query(query):
 rows_ads = run_query(f'SELECT * FROM "{link_ads}"')
 ads = pd.DataFrame(rows_ads, dtype=str)
 st.dataframe(data = ads)
-
-
-import requests
-
-def download_file_from_google_drive(id, destination):
-    URL = link_ads
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)    
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-
-if __name__ == "__main__":
-    file_id = 'TAKE ID FROM SHAREABLE LINK'
-    destination = 'DESTINATION FILE ON YOUR DISK'
-    download_file_from_google_drive(file_id, destination)
