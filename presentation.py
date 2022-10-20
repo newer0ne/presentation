0d2b4283266f451ed2172c3921a1ff2c895fb7de
Newer0ne <t80448@gmail.com>
2022-10-20 12:36:57 +0600

    Added test task

--- presentation.py
+++ presentation.py
@@ -1,10 +1,4 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns(3)
-with col2:
-    st.image("photo.jpg")
-
-st.title('Группа Автоматизации')
-st.write('Проект создания Группы Автоматизации')
-st.write('в составе Отдела Инновационных Технологий')
-st.write('Проектно Конструкторской Группы АО “КОНЦЕРН ТИТАН-2”')
+link_test_task = '[Тестовое задание](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)'
+st.markdown(link_test_task, unsafe_allow_html=True)
