a6a092621a3e9bc3a955a581adc8926a62e67eaf
Newer0ne <t80448@gmail.com>
2022-10-20 12:55:18 +0600

    added gdrive download script

--- presentation.py
+++ presentation.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 link_test_task = '[Тестовое задание](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)'
 st.markdown(link_test_task, unsafe_allow_html=True)
@@ -10,3 +11,40 @@ def run_query(query):
 rows_ads = run_query(f'SELECT * FROM "{link_ads}"')
 ads = pd.DataFrame(rows_ads, dtype=str)
 st.dataframe(data = ads)
+
+
+import requests
+
+def download_file_from_google_drive(id, destination):
+    URL = link_ads
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+if __name__ == "__main__":
+    file_id = 'TAKE ID FROM SHAREABLE LINK'
+    destination = 'DESTINATION FILE ON YOUR DISK'
+    download_file_from_google_drive(file_id, destination)
