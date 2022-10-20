79f839e2f511d5458d967984801d40d9d98ea7e9
Newer0ne <t80448@gmail.com>
2022-10-20 12:46:38 +0600

    added link on table with ads

--- presentation.py
+++ presentation.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 link_test_task = '[Тестовое задание](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)'
 st.markdown(link_test_task, unsafe_allow_html=True)
+
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+rows_ads = run_query(f'SELECT * FROM "{https://drive.google.com/file/d/1U7DQGPOEhGWW3aWQHZayF_9ScqBYnaE1/}"')
+ads = pd.DataFrame(rows_ads, dtype=str)
+st.dataframe(data = ads)
