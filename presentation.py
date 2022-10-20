794a40abdadcf0d83f0d6769f8ef02714c2880c1
Newer0ne <t80448@gmail.com>
2022-10-20 19:39:54 +0600

    2

--- presentation.py
+++ presentation.py
@@ -33,7 +33,7 @@ def to_excel(df):
     return processed_data
 
 
-link_Dynam_video_toclic = '[<h5 style='text-align: center;'>Тестовое задание:</h5>"""](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)'
+link_Dynam_video_toclic = '["""<h5 style='text-align: center;'>Тестовое задание:</h5>"""](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)'
 st.markdown(link_Dynam_video_toclic, unsafe_allow_html=True)
 
 st.markdown("""<h5 style='text-align: center;'>Первый датасет ADS:</h5>""", unsafe_allow_html = True)
