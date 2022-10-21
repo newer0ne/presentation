d57dd1493a17a3aea5e08f008d8085b20bdfc2f7
Newer0ne <t80448@gmail.com>
2022-10-21 20:22:52 +0600

    1

--- presentation.py
+++ presentation.py
@@ -37,14 +37,27 @@ def to_excel(df):
 link_Dynam_video_toclic = """[<h2 style='text-align: center;'>Тестовове задание:</h2>](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)"""
 st.markdown(link_Dynam_video_toclic, unsafe_allow_html=True)
 
+load_option = st.selectbox(
+        "Выбор способа загрузки датасета",
+        ("open", "link", "upload"))
+
 st.markdown("""<h5 style='text-align: center;'>Первый датасет ADS:</h5>""", unsafe_allow_html = True)
 
-url_ads = st.secrets["ads"]
-file_id_ads = url_ads.split('/')[-2]
-dwn_url_ads = 'https://drive.google.com/uc?export=download&id=' + file_id_ads
-url2_ads = requests.get(dwn_url_ads).text
-csv_raw_ads = StringIO(url2_ads)
-df_ads = pd.read_csv(csv_raw_ads)
+if load_option == "open"
+    df_ads = pd.read_csv(ads.csv)
+elif load_option == "link"
+    url_ads = st.secrets["ads"]
+    file_id_ads = url_ads.split('/')[-2]
+    dwn_url_ads = 'https://drive.google.com/uc?export=download&id=' + file_id_ads
+    url2_ads = requests.get(dwn_url_ads).text
+    csv_raw_ads = StringIO(url2_ads)
+    df_ads = pd.read_csv(csv_raw_ads)
+elif load_option == "dowload"
+    uploaded_file = st.file_uploader("Область загрузки CSV")
+        if uploaded_file is not None:
+        st.write("Filename: ", uploaded_file.name)
+        df_ads = pd.read_csv(uploaded_file, sheet_name = "Sheet1", dtype = {'Note': str})
+
 st.dataframe(df_ads)
 
 cols_ads = df_ads.columns
@@ -69,11 +82,14 @@ for i in range(len(cols_leads)):
 
 st.markdown("""<h5 style='text-align: center;'>Третий датасет PURCHASES:</h5>""", unsafe_allow_html = True)
 
+
+
 url_purchases = st.secrets["purchases"]
 file_id_purchases = url_purchases.split('/')[-2]
 dwn_url_purchases = 'https://drive.google.com/uc?export=download&id=' + file_id_purchases
 url2_purchases = requests.get(dwn_url_purchases).text
 csv_raw_purchases = StringIO(url2_purchases)
+
 df_purchases = pd.read_csv(csv_raw_purchases)
 #df_purchases = df_purchases.reindex(columns=['client_id', 'purchase_id', 'purchase_created_at', 'm_purchase_amount'])
 st.dataframe(df_purchases)
