88a3e14b5c62552440d59214a5c64791979af56e
Newer0ne <t80448@gmail.com>
2022-10-22 10:44:20 +0600

    1

--- presentation.py
+++ presentation.py
@@ -42,15 +42,29 @@ load_option = st.radio(
     "Выбор способа загрузки датасета",
     ("open", "link", "upload"))
 
-st.markdown("""<h5 style='text-align: center;'>Первый датасет ADS:</h5>""", unsafe_allow_html = True)
-
 if load_option == "open":
     df_ads = pd.read_csv("ads.csv")
+    st.markdown("""<h5 style='text-align: center;'>Первый датасет ADS:</h5>""", unsafe_allow_html = True)
     st.dataframe(df_ads)
     cols_ads = df_ads.columns
     list_ads = []
     for i in range(len(cols_ads)):
         list_ads.append(cols_ads[i])
+    
+    df_leads = pd.read_csv("leads.csv")
+    st.markdown("""<h5 style='text-align: center;'>Второй датасет LEADS:</h5>""", unsafe_allow_html = True)
+    st.dataframe(df_leads)
+    cols_leads = df_leads.columns
+    list_leads = []
+    for i in range(len(cols_leads)):
+        list_leads.append(cols_leads[i])
+    
+    df_purchases = pd.read_csv("purchases.csv")
+    st.dataframe(df_purchases)
+    cols_purchases = df_purchases.columns
+    list_purchases = []
+    for i in range(len(cols_purchases)):
+        list_purchases.append(cols_purchases[i])
 
 elif load_option == "link":
     url_ads = st.secrets["ads"]
@@ -59,58 +73,73 @@ elif load_option == "link":
     url2_ads = requests.get(dwn_url_ads).text
     csv_raw_ads = StringIO(url2_ads)
     df_ads = pd.read_csv(csv_raw_ads)
+    st.markdown("""<h5 style='text-align: center;'>Первый датасет ADS:</h5>""", unsafe_allow_html = True)
     st.dataframe(df_ads)
     cols_ads = df_ads.columns
     list_ads = []
     for i in range(len(cols_ads)):
         list_ads.append(cols_ads[i])
+    
+    url_leads = st.secrets["leads"]
+    file_id_leads = url_leads.split('/')[-2]
+    dwn_url_leads = 'https://drive.google.com/uc?export=download&id=' + file_id_leads
+    url2_leads = requests.get(dwn_url_leads).text
+    csv_raw_leads = StringIO(url2_leads)
+    df_leads = pd.read_csv(csv_raw_leads)
+    st.markdown("""<h5 style='text-align: center;'>Второй датасет LEADS:</h5>""", unsafe_allow_html = True)
+    st.dataframe(df_leads)
+    cols_leads = df_leads.columns
+    list_leads = []
+    for i in range(len(cols_leads)):
+	    list_leads.append(cols_leads[i])
+    
+    url_purchases = st.secrets["purchases"]
+    file_id_purchases = url_purchases.split('/')[-2]
+    dwn_url_purchases = 'https://drive.google.com/uc?export=download&id=' + file_id_purchases
+    url2_purchases = requests.get(dwn_url_purchases).text
+    csv_raw_purchases = StringIO(url2_purchases)
+    df_purchases = pd.read_csv(csv_raw_purchases)
+    #df_purchases = df_purchases.reindex(columns=['client_id', 'purchase_id', 'purchase_created_at', 'm_purchase_amount'])
+    st.markdown("""<h5 style='text-align: center;'>Третий датасет PURCHASES:</h5>""", unsafe_allow_html = True)
+    st.dataframe(df_purchases)
+    cols_purchases = df_purchases.columns
+    list_purchases = []
+    for i in range(len(cols_purchases)):
+    	list_purchases.append(cols_purchases[i])
 
 elif load_option == "upload":
-    uploaded_file = st.file_uploader("Область загрузки CSV")
-    if uploaded_file is not None:
-        st.write("Filename: ", uploaded_file.name)
-        df_ads = pd.read_csv(uploaded_file, sheet_name = "Sheet1", dtype = {'Note': str})
+    uploaded_ads = st.file_uploader("Область загрузки для ADS.CSV")
+    if uploaded_ads is not None:
+        st.write("Filename: ", uploaded_ads.name)
+        df_ads = pd.read_csv(uploaded_ads, sheet_name = "Sheet1", dtype = {'Note': str})
+        st.markdown("""<h5 style='text-align: center;'>Первый датасет ADS:</h5>""", unsafe_allow_html = True)
         st.dataframe(df_ads)
         cols_ads = df_ads.columns
         list_ads = []
         for i in range(len(cols_ads)):
             list_ads.append(cols_ads[i])
 
+    uploaded_leads = st.file_uploader("Область загрузки для LEADS.CSV")
+    if uploaded_leads is not None:
+        st.write("Filename: ", uploaded_leads.name)
+        df_leads = pd.read_csv(uploaded_leads, sheet_name = "Sheet1", dtype = {'Note': str})
+        st.markdown("""<h5 style='text-align: center;'>Второй датасет LEADS:</h5>""", unsafe_allow_html = True)
+        st.dataframe(df_leads)
+        cols_leads = df_leads.columns
+        list_leads = []
+        for i in range(len(cols_leads)):
+            list_leads.append(cols_leads[i])
 
-st.markdown("""<h5 style='text-align: center;'>Второй датасет LEADS:</h5>""", unsafe_allow_html = True)
-
-url_leads = st.secrets["leads"]
-file_id_leads = url_leads.split('/')[-2]
-dwn_url_leads = 'https://drive.google.com/uc?export=download&id=' + file_id_leads
-url2_leads = requests.get(dwn_url_leads).text
-csv_raw_leads = StringIO(url2_leads)
-df_leads = pd.read_csv(csv_raw_leads)
-st.dataframe(df_leads)
-
-cols_leads = df_leads.columns
-list_leads = []
-for i in range(len(cols_leads)):
-	list_leads.append(cols_leads[i])
-
-st.markdown("""<h5 style='text-align: center;'>Третий датасет PURCHASES:</h5>""", unsafe_allow_html = True)
-
-
-
-url_purchases = st.secrets["purchases"]
-file_id_purchases = url_purchases.split('/')[-2]
-dwn_url_purchases = 'https://drive.google.com/uc?export=download&id=' + file_id_purchases
-url2_purchases = requests.get(dwn_url_purchases).text
-csv_raw_purchases = StringIO(url2_purchases)
-
-df_purchases = pd.read_csv(csv_raw_purchases)
-#df_purchases = df_purchases.reindex(columns=['client_id', 'purchase_id', 'purchase_created_at', 'm_purchase_amount'])
-st.dataframe(df_purchases)
-
-cols_purchases = df_purchases.columns
-list_purchases = []
-for i in range(len(cols_purchases)):
-	list_purchases.append(cols_purchases[i])
-
+    uploaded_purchases = st.file_uploader("Область загрузки для PURCHASES.CSV")
+    if uploaded_purchases is not None:
+        st.write("Filename: ", uploaded_purchases.name)
+        df_purchases = pd.read_csv(uploaded_purchases, sheet_name = "Sheet1", dtype = {'Note': str})
+        st.markdown("""<h5 style='text-align: center;'>Второй датасет LEADS:</h5>""", unsafe_allow_html = True)
+        st.dataframe(df_purchases)
+        cols_purchases = df_purchases.columns
+        list_purchases = []
+        for i in range(len(cols_purchases)):
+            list_purchases.append(cols_purchases[i])
 
 col1, col2 = st.columns(2)
 
