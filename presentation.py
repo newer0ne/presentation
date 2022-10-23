7f43097c502c53a9da0d8936e26a1f43eb913c58
Newer0ne <t80448@gmail.com>
2022-10-24 02:23:49 +0600

    1

--- presentation.py
+++ presentation.py
@@ -249,4 +249,4 @@ with st.expander("Dataset Filter"):
         df_filter2["purchase_created_at"] = pd.to_datetime(df_filter2["purchase_created_at"])
         df_filter2["lead_created_at"] = pd.to_datetime(df_filter2["lead_created_at"])
         df_filter2['datelag'] = (df_filter2['purchase_created_at'] - df_filter2['lead_created_at'])
-        df_filter2
+        st.dataframe(df_filter2)
