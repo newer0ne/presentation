4d8802925a44be3e5ba6e8d25d49e180070a5d94
Newer0ne <t80448@gmail.com>
2022-10-24 01:58:32 +0600

    1

--- presentation.py
+++ presentation.py
@@ -252,5 +252,6 @@ with st.expander("Dataset Filter"):
         df_filter2['datelag'] = 0
         st.text("added datelag")
         df_filter2
-        df_filter2['datelag'] = df_filter2.purchase_created_at - df_filter2.lead_created_at
+        #df_filter2['datelag'] = df_filter2.purchase_created_at - df_filter2.lead_created_at
+        df_filter2['datelag'] = (df_filter2['purchase_created_at'] - df_filter2['lead_created_at']).dt.days
         df_filter2.sort_values(by = ['datelag'])
