909ed20b2e68ca89fc3b9b349e071d994003d912
Newer0ne <t80448@gmail.com>
2022-10-24 00:44:48 +0600

    1

--- presentation.py
+++ presentation.py
@@ -4,13 +4,8 @@ import streamlit as st
 from gsheetsdb import connect
 import pandas as pd
 import numpy as np
-import openpyxl
 import io
 from io import BytesIO
-import os
-import csv
-from pyxlsb import open_workbook as open_xlsb
-from PIL import Image
 import requests
 from io import StringIO
 
@@ -19,20 +14,6 @@ conn = connect()
 def run_query(query):
     rows = conn.execute(query, headers=1)
     return rows
-
-
-def to_excel(df):
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0,00'})
-    worksheet.set_column('A:A', None, format1)
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
-
 
 link_Dynam_video_toclic = """[<h2 style='text-align: center;'>Тестовове задание:</h2>](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)"""
 st.markdown(link_Dynam_video_toclic, unsafe_allow_html=True)
