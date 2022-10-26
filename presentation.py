99a725dc0121727b548fe9f7bccedd5dfde005e4
Newer0ne <t80448@gmail.com>
2022-10-27 00:49:13 +0600

    1

--- presentation.py
+++ presentation.py
@@ -35,7 +35,7 @@ class Dataset:
         self.name = index
         for i in range(len(self.df.columns)):
             self.listcols.append(self.df.columns[i])
-        self.listcols
+        st.text(self.listcols)
 
     def linkup(self, index):
         #link_url = st.secrets[self.link]
