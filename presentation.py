00bb1af184792e6103ee957ebbcf19efb45270a5
Newer0ne <t80448@gmail.com>
2022-10-27 17:56:25 +0600

    1

--- presentation.py
+++ presentation.py
@@ -172,7 +172,7 @@ with anal2:
         "Columns x selection 👉",
         (data2.listcols), key = "anal2")
     st.write("Choosed x column: " + anal_col2)
-    st.line_chart(data2.df.loc[anal_col2])
+    st.line_chart(data2.df.loc[:, anal_col2])
 
 with anal3:
     st.write("Choosed dataframe: " + data3.name)
@@ -180,7 +180,7 @@ with anal3:
         "Columns x selection 👉",
         (data3.listcols), key = "anal3")
     st.write("Choosed x column: " + anal_col3)
-    st.line_chart(data3.df.loc[anal_col3])
+    st.line_chart(data3.df.loc[:, anal_col3])
 
 with anal4:
     st.write("Choosed dataframe: " + data4.name)
@@ -188,7 +188,7 @@ with anal4:
         "Columns x selection 👉",
         (data4.listcols), key = "anal4")
     st.write("Choosed x column: " + anal_col4)
-    st.line_chart(data4.df.loc[anal_col4])
+    st.line_chart(data4.df.loc[:, anal_col4])
     
 
 
