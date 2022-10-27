958ed98a27097fb715fa5e0698af7811eaceb2d8
Newer0ne <t80448@gmail.com>
2022-10-27 14:09:36 +0600

    1

--- presentation.py
+++ presentation.py
@@ -130,7 +130,7 @@ with st.expander("Dataframe Renamer"):
     with ren3:
         newcolname = st.text_input('New column name', ren_col)
         st.write("New name for the selected column: " + newcolname)
-        print(type(ren_col))
+        st.text(type(ren_col))
         if ren_df == name_list[0]:
             data1.df[ren_col] = data1.df[newcolname]
             data1.df[newcolname]
