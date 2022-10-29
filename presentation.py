1654a4b584865b3aff3bb47248678b04fa3c4c0c
Newer0ne <t80448@gmail.com>
2022-10-29 16:38:19 +0600

    1

--- presentation.py
+++ presentation.py
@@ -131,6 +131,7 @@ if load_option == opt_desc[0]:
         data2.open(name_list[1])
 
         del data2.df['utm_term']
+        del data2.df['utm_medium']
         data2.df = data2.df[(data2.df['utm_source'] == 'yandex') & (data2.df['utm_medium'] == 'cpc')]
         data2.df['client_id'] = data2.df['client_id'].astype(str)
         data2.df = data2.df[(data2.df['client_id'] != 'nan')]
