0a0ba60f2c2cb87e8b88c19444e5ecd28b80dcaa
Newer0ne <t80448@gmail.com>
2022-10-29 15:25:52 +0600

    1

--- presentation.py
+++ presentation.py
@@ -129,7 +129,8 @@ if load_option == opt_desc[0]:
     with tab_open2:
         data2.open(name_list[1])
 
-        data2.df = data2.df[data2.df['utm_source'] == 'yandex']
+        del data2.df['utm_term']
+        data2.df = data2.df[(data2.df['utm_source'] == 'yandex') & (data2.df['utm_medium'] == 'cpc') & (data2.df['client_id'] is not None)]
 
         buffer2 = io.StringIO()
         data2.dfi = data2.df
