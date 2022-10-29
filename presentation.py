5955896ae4503592af38d3a6c6a5e5975795cd65
Newer0ne <t80448@gmail.com>
2022-10-29 13:15:20 +0600

    1

--- presentation.py
+++ presentation.py
@@ -175,6 +175,7 @@ maxUploadSize = 400
 
 m23 = pd.merge(data2.df, data3.df, how = 'left', on = 'client_id')
 m23f = m23[(m23.utm_source == 'yandex') & (m23.m_purchase_amount > 0)]
+m23f.astype({'m_purchase_amount': 'int'})
 st.dataframe(m23f)
 buffer4 = io.StringIO()
 m23i = m23f
