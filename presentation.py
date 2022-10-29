492ce9babb0ab37d1436e2a190de6a79fc7aa821
Newer0ne <t80448@gmail.com>
2022-10-29 16:04:46 +0600

    1

--- presentation.py
+++ presentation.py
@@ -134,7 +134,8 @@ if load_option == opt_desc[0]:
         data2.df = data2.df[(data2.df['utm_source'] == 'yandex') & (data2.df['utm_medium'] == 'cpc')]
         #data2.df = data2.df[(data2.df['utm_campaign'] != 'nan')]
         data2.df['client_id'] = data2.df['client_id'].astype(str)
-        data2.df = data2.df[(data2.df['client_id'] != 'nan')]
+        data2.df = data2.df[(data2.df['client_id'] != 'nan')]        
+        data2.df = data2.df[(data2.df['utm_content'].notnull())]
 
         buffer2 = io.StringIO()
         data2.dfi = data2.df
