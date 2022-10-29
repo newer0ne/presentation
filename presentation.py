d882848a8c3614a2413f1f2a0222a01dbec99c3f
Newer0ne <t80448@gmail.com>
2022-10-29 14:50:16 +0600

    1

--- presentation.py
+++ presentation.py
@@ -97,7 +97,7 @@ if load_option == opt_desc[0]:
     with tab_open1:
         data1.open(name_list[0])
         data1.df['m_clicks'] = data1.df['m_clicks'].astype(int)
-        del data1.df['m_clicks']
+        #del data1.df['m_clicks']
         data1.df['m_cost_int'] = data1.df['m_cost'].astype(int)
         del data1.df['m_cost']
         st.dataframe(data1.df)
