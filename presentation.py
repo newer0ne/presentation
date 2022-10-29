791bd30ec231d3a7a8caaa43afec9278fcd4d6ae
Newer0ne <t80448@gmail.com>
2022-10-29 14:15:29 +0600

    1

--- presentation.py
+++ presentation.py
@@ -175,6 +175,7 @@ maxUploadSize = 400
 
 m23 = pd.merge(data2.df, data3.df, how = 'left', on = 'client_id')
 st.text("Фильтрованная таблица:")
+m23['m_purchase_amount'].astype(str).astype(int)
 m23f = m23[(m23.utm_source == 'yandex') & (m23.m_purchase_amount > 0)]
 st.text("Astype():")
 m23f.astype({'m_purchase_amount': 'int64'})
