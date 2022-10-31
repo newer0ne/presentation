0c5c122b77177e37015c02c1fd843406b1bce1b0
Newer0ne <t80448@gmail.com>
2022-10-31 14:30:08 +0600

    1

--- presentation.py
+++ presentation.py
@@ -104,8 +104,8 @@ if load_option == opt_desc[0]:
         data1.Open(name_list[0])
         data1.DFinfo()
 
-        for i in range(len(data2.listcols)):
-            st.write(print(i))
+        for i in range(len(data1.listcols)):
+            st.write(data1.listcols[i])
             #data2.df.iloc[:, i].unique()
             #i += 1
         
