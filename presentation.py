8e8bab28fb54d8bf35b51663fabf37af234a595b
Newer0ne <t80448@gmail.com>
2022-10-31 12:39:43 +0600

    1

--- presentation.py
+++ presentation.py
@@ -121,9 +121,11 @@ if load_option == opt_desc[0]:
 #        st.dataframe(data2.df)
 
 # Пригодится под цикл уникальных значений в столбцах
-# for i in range(len(self.listcols)):
-#        self.listcols = st.columns(len(self.listcols))
-
+ for i in range(len(data2.listcols)):
+        data2.listcols = st.columns(len(data2.listcols))
+#        for i in data2.listcols
+#        data2.listcols[0] = 1
+#
 #        self.colop1.write("Уникальные значения в столбце 1: ")
 #        self.colop1.dataframe(self.df.iloc[:, 0].unique())
 #        self.colop2.write("Уникальные значения в столбце 2: ")
