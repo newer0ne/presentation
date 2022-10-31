57b13913ccf42e115ecc7467ebf69a7c4bf9bcb2
Newer0ne <t80448@gmail.com>
2022-10-31 12:48:41 +0600

    1

--- presentation.py
+++ presentation.py
@@ -121,8 +121,8 @@ if load_option == opt_desc[0]:
 #        st.dataframe(data2.df)
 
 # Пригодится под цикл уникальных значений в столбцах
-for i in range(len(data2.df.columns)):
-    data2.listcols = st.columns(len(data2.listcols))
+        for i in range(len(data2.df.columns)):
+            data2.listcols = st.columns(len(data2.listcols))
 
 #        for i in data2.listcols
 #        data2.listcols[0] = 1
