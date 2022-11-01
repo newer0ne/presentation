f15126dbe10fe5a211d597094fe6d45a651da926
Newer0ne <t80448@gmail.com>
2022-11-01 13:49:37 +0600

    1

--- presentation.py
+++ presentation.py
@@ -86,7 +86,7 @@ class Dataset:
         unicols = st.columns(x)
         for ii in range(x):
             unicols[ii].text(self.listcols[ii])
-            st.write(unicols[ii](self.df.iloc[:, ii].unique()))
+            st.write(self.df.iloc[:, ii].unique())
 
     def renamecol(self, oldname, newname):
         self.df[newname] = self.df[oldname]
