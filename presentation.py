66e71e3c932c698b796ae49172ebf2a658d8117a
Newer0ne <t80448@gmail.com>
2022-11-04 12:49:22 +0600

    1

--- presentation.py
+++ presentation.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Hello",
-    page_icon="👋",
+    page_icon="📖",
 )
 
 st.write("# Welcome to Datalyzer! 👋")
@@ -18,7 +18,7 @@ st.markdown(
         [telegramm.org](https://t.me/newer0ne)
         & [vk.com](https://vk.com/id1237712)
     - Or pages on recruiting platforms 
-        [headHunter.ru](https://hh.ru/resume/7e8af31aff09d92df80039ed1f674457646c75) 
+        [headhunter.ru](https://hh.ru/resume/7e8af31aff09d92df80039ed1f674457646c75) 
         & [career.habr.com](https://career.habr.com/sergeyzaharov123)
     - Ask any other question by email [email](work.sergeyzaharov@gmail.com)
 
