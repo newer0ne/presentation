678d6ec15cd8592d813ccfcc35fa33683dea2c6f
Newer0ne <t80448@gmail.com>
2022-11-09 17:45:47 +0600

    1

--- pages/6_project.py
+++ pages/6_project.py
@@ -34,19 +34,5 @@ st.markdown(
         Python Streamlit;
     -   поддержание текущего и разработка последующих планов разработки ОПС КТ2.
 
-
-    ### Хотите знать немного больше об авторе?
-    - Мои работы:
-        - [Классификатор для РОСАТОМА](https://classificator.streamlit.app/)
-        - [Атесстация в ИТМО](https://itmofinishwork.streamlitapp.com/)
-    - Здесь мои социальные сети:
-        - [telegram.org](https://t.me/newer0ne)
-        - [linkedin.com](https://www.linkedin.com/in/sergey-zakharov-162331223/)
-        - [vk.com](https://vk.com/id1237712)
-        - [github.com](https://github.com/newer0ne)
-    - Контакты на рекрутинговых платформах:
-        - [headhunter.ru](https://hh.ru/resume/7e8af31aff09d92df80039ed1f674457646c75)
-        - [career.habr.com](https://career.habr.com/sergeyzaharov123)
-    - Если возникли какие-то вопросы моя электрнная почта work.sergeyzaharov@gmail.com
 """
 )
