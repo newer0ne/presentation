a0ad9db23571eb879a0edc40eae7b3369172fbcd
Newer0ne <t80448@gmail.com>
2022-11-21 02:17:17 +0600

    1

--- pages/1_📄_ADS analyzer.py
+++ pages/1_📄_ADS analyzer.py
@@ -15,6 +15,9 @@ st.write(
 """
 )
 
+tasklink = """[Тестовое задание.](https://xoservices.notion.site/1872d331265946a0ae2c5c9069189fd7)"""
+st.markdown(tasklink, unsafe_allow_html=True)
+
 upcol1, upcol2, upcol3 = st.columns(3)
 
 with upcol1:
