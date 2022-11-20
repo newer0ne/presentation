d69f4bc15a642892a5b49720746803d08d62781b
Newer0ne <t80448@gmail.com>
2022-11-20 13:52:35 +0600

    1

--- pages/6_📊_Test_task.py
+++ pages/6_📊_Test_task.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Test task solution", page_icon="📊", initial_sidebar_state="expanded"
+    page_title="Test task solution", page_icon="📊", layout="wide", initial_sidebar_state="expanded"
 )
 
 
