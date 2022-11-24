93bdf31e810f4fbd5d9fdea88011c367dcb50166
Newer0ne <t80448@gmail.com>
2022-11-24 21:24:23 +0600

    1

--- pages/1_📄_ADS analyzer.py
+++ pages/1_📄_ADS analyzer.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from io import StringIO
 
 st.set_page_config(
-    page_title="Test task in work", page_icon="📊", layout="wide", initial_sidebar_state="expanded"
+    page_title="ADS analyzer", page_icon="📊", layout="wide", initial_sidebar_state="expanded"
 )
 
 st.write(
