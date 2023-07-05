f09220bf3652c8457a87255b1d2d5c27aeef5f9b
Newer0ne <t80448@gmail.com>
2023-07-06 00:28:04 +0300

    1

--- pages/5_📊_Concentrate.py
+++ pages/5_📊_Concentrate.py
@@ -85,7 +85,7 @@ exceed_limit = []
 for column in calc_mat.columns[2:]:
     element_symbol.append(column)
     element_mass.append(calc_mat[column].values[0])
-    element_pdk.append(pdk[element_symbol])
+    element_pdk.append(pdk[column])
     exceed_limit = element_mass > element_pdk
     
 print(element_symbol)
