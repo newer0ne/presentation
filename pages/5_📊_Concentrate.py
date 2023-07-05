28f05e62cb6767a76f8dcbe6321dfb2496085bb3
Newer0ne <t80448@gmail.com>
2023-07-05 23:56:39 +0300

    1

--- pages/5_📊_Concentrate.py
+++ pages/5_📊_Concentrate.py
@@ -74,15 +74,15 @@ pdk = {
     'Mo': 0.05
 }
 
+compare_df = pd.DataFrame(columns=['Элемент', 'Масса', 'ПДК', 'Превышение'])
+
 for column in calc_mat.columns[2:]:
     element_symbol = column
     element_mass = calc_mat[column].values[0]
     element_pdk = pdk[element_symbol]
-    exceed_limit = element_mass > element_pdk
-
-    compare_df = compare_df.append({
+    exceed_limit = element_mass > element_p compare_df = compare_df.append({
         'Элемент': element_symbol,
-        'Масса': element_mass,
+ 'Масса': element_mass,
         'ПДК': element_pdk,
         'Превышение': exceed_limit
     }, ignore_index=True)
